2c6c04ce1659484a25a8768f07a457b9ac0c66fa
scgeier <scgeier@gmail.com>
2015-09-25 17:29:11 -0400

    second draft

--- js/scripts.js
+++ js/scripts.js
@@ -14,19 +14,56 @@ $(document).ready(function(){
   
   $('.baseball').qtip({ // Grab some elements to apply the tooltip to
     content: {
-      text: '<img src="../images/hoss.jpg" />',
-      title:'The First Flipped Bird'
+      text: '<img src="./images/hoss.jpg" width=400 height=600 />',
+      title:'The First Flipped Bird',
+      hide:'unfocus'
       },
     position: {
-        my: 'top right',  // Position my top left...
-        at: 'bottom left', // at the bottom right of...
+        my: 'center right',  // Position my top left...
+        at: 'top left', // at the bottom right of...
         target: $('.baseball') // my target
     },
         style: {
-        classes: 'qtip-blue qtip-shadow qtip-rounded'
+        classes: 'qtip-dark qtip-shadow qtip-rounded',
     }
-    
+  
 })
+  
+    $('.swear').qtip({ // Grab some elements to apply the tooltip to
+    content: {
+      text: '<img src="./images/swear.jpg" />',
+      title:'Pinky Swear',
+      hide:'unfocus'
+      },
+    position: {
+        my: 'top right',  // Position my top left...
+        at: 'top left', // at the bottom right of...
+        target: $('.swear') // my target
+    },
+        style: {
+        classes: 'qtip-dark qtip-shadow qtip-rounded',
+    }
+  
+})
+        $('.ratio').qtip({ // Grab some elements to apply the tooltip to
+    content: {
+      text: '<img src="./images/ratio.png" />',
+      title:'The 2D:4D Ratio',
+      hide:'unfocus'
+      },
+    position: {
+        my: 'top right',  // Position my top left...
+        at: 'top left', // at the bottom right of...
+        target: $('.ratio') // my target
+    },
+        style: {
+        classes: 'qtip-dark qtip-shadow qtip-rounded',
+    }
+  
+})
+        
+  jQuery('.headline').fitText(0.9);
+  jQuery('#subtitle').fitText(0.9);
 
 });
 
